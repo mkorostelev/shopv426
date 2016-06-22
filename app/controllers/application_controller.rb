478d0ba5e8b76323010f91ebf6d9d735498d1f88
Maks Korostelov <kostul89@gmail.com>
2016-06-22 23:30:03 +0300

    Litle changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,7 +56,6 @@ class ApplicationController < ActionController::Base
     #code
   end
 
-  private
   def authenticate
     authenticate_or_request_with_http_token do |token, options|
       @current_user = User.joins(:auth_token).find_by(auth_tokens: { value: token })
